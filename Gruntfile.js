0a4365b65ad758d03b2f01c20bcbcb38d1c75f52
Todd Johnson <todd@flingo.tv>
2015-03-20 20:10:26 -0700

    adding files which manage dependancies, tasks, and publishing policy

---
+++ Gruntfile.js
@@ -0,0 +1,70 @@
+module.exports = function(grunt) {
+
+    // Project configuration.
+    grunt.initConfig({
+
+        pkg : grunt.file.readJSON('package.json'),
+
+        src : ["src/**/*.js"],
+
+        uglify : {
+            options : {
+                banner : "/*! <%= pkg.title %> v<%= pkg.version %> | build date : <%= grunt.template.today('yyyy-mm-dd') %> */\n",
+                mangle : true
+            },
+
+            min : {
+                options : {
+                    compress : true,
+                    beautify : false,
+                    compress : {
+                        drop_console : true
+                    }
+                },
+                files : {
+                    "dist/<%= pkg.name %>.min.js" : "<%= src %>"
+                }
+            },
+
+            regular : {
+                options : {
+                    mangle   : false,
+                    compress : false,
+                    beautify : true
+                },
+                files : {
+                    "dist/<%= pkg.name %>.js" : "<%= src %>"
+                }
+            }
+        },
+
+        yuidoc : {
+            compile : {
+                name        : "<%= pkg.name %>",
+                description : "<%= pkg.description %>",
+                version     : "<%= pkg.version %>",
+                url         : "<%= pkg.homepage %>",
+                options     : {
+                    paths : [
+                        "src/"
+                    ],
+                    outdir : "dist/docs/"
+                }
+            }
+        },
+
+        bump : {
+            options : {
+                files         : ['package.json'],
+                updateConfigs : ['pkg']
+            }
+        }
+    });
+
+    grunt.loadNpmTasks("grunt-contrib-yuidoc");
+    grunt.loadNpmTasks("grunt-contrib-uglify");
+    grunt.loadNpmTasks("grunt-bump");
+
+    // Default task(s).
+    grunt.registerTask("default", ["uglify", "yuidoc"]);
+};
