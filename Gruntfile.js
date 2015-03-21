9634bde43fce00781fd1635b7032db9d04326961
Todd Johnson <tjohnson4@gmail.com>
2015-03-20 22:54:51 -0700

    update bower dependancies

--- Gruntfile.js
+++ Gruntfile.js
@@ -55,8 +55,8 @@ module.exports = function(grunt) {
 
         bump : {
             options : {
-                files         : ['package.json'],
-                updateConfigs : ['pkg']
+                files:         ['package.json', 'bower.json'],
+                updateConfigs: ['pkg']
             }
         }
     });
